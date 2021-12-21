e38a5ebeb1b1adc54a7eae5449d57403df2ab2f2
unknown <paomil999@gmail.com>
2021-12-20 22:38:28 -0500

    feat: creating users list without styles

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        <table>
+                            <tr>
+                                <td>Id</td>
+                                <td>Name</td>
+                                <td>Email</td>
+                            </tr>
+                            @foreach($users as $user):
+                            <tr>
+                                <td>{{$user->id}}</td>
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->email}}</td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+
+
