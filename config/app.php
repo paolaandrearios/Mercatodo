e472c9896e2a3a9f53d89a9e5d67bd4b37f92237
paolaandrearios <paomil999@gmail.com>
2022-04-18 20:17:29 -0500

    fix: Solve bug in  laravel excel installation enabling 'extension=gd' in  file php.ini

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class
 
     ],
 
@@ -234,7 +235,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Helper' => \App\Helpers\Helper::class,
-
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
